8543bfdbcf49c77b33c41a9ccccf137be27a2cfe
GHTeodor <Theodor.Lviv@gmail.com>
2022-08-26 21:20:15 +0300

    add-migration UsersMigration

---
+++ Quiz-PROJECT/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quiz_PROJECT.Models;
+
+namespace Quiz_PROJECT;
+
+public class DBContext : DbContext
+{
+    public DBContext(DbContextOptions<DBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ Quiz-PROJECT/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Quiz_PROJECT.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string NickName { get; set; }
+}
