3985a88d4717bf49d684454d12d3f68abe9bf007
GHTeodor <Theodor.Lviv@gmail.com>
2022-09-19 00:12:41 +0300

    SwaggerAuthorize

---
+++ Quiz-PROJECT/Configurations/SwaggerBearer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Quiz_PROJECT.Configurations;
+
+public static class SwaggerBearer
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test Identity Server API", Version = "v1" });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description =
+                    "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+    }
+}
